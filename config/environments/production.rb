01c0c439b818539406bb9e147aa52768a55775b0
massatoMMKP <massato.kanenoo@gmail.com>
2026-04-27 00:37:39 -0300

    chore: enable solid suite using primary database connection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,10 +48,13 @@ Rails.application.configure do
 
   # Replace the default in-process memory cache store with a durable alternative.
   config.cache_store = :solid_cache_store
+  config.solid_cache.connects_to = { database: { writing: :cache } }
 
   # Replace the default in-process and non-durable queuing backend for Active Job.
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
+
+  config.solid_cable.connects_to = { database: { writing: :cable } }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
