2a9041af7d2538ab389fd77984b1b33f115055f1
massatoMMKP <massato.kanenoo@gmail.com>
2026-04-27 00:56:02 -0300

    testing

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module PhotoApp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 8.0
 
-    config.assets.initialize_on_precompile = false
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -23,7 +22,7 @@ module PhotoApp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    # config.time_zone = "Central Time (US & Canada)"he amle 
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
